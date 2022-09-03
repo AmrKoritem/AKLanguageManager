4199d1ea9c957d929a1954ad04ee98e4ef3ca49a
Amr Koritem <amr.koritem92@gmail.com>
2022-09-04 00:28:20 +0200

    Initial Commit

---
+++ AKLanguageManager/AKLanguageManager.h
@@ -0,0 +1,18 @@
+//
+//  AKLanguageManager.h
+//  AKLanguageManager
+//
+//  Created by Amr Koritem on 04/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKLanguageManager.
+FOUNDATION_EXPORT double AKLanguageManagerVersionNumber;
+
+//! Project version string for AKLanguageManager.
+FOUNDATION_EXPORT const unsigned char AKLanguageManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKLanguageManager/PublicHeader.h>
+
+
