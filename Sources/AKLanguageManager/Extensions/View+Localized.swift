08f2dd36c831e75bda5188e6ce91b41e66e7085d
Amr Koritem <amr.koritem92@gmail.com>
2022-10-08 17:03:12 +0200

    spm issue fixed

--- Sources/AKLanguageManager/Extensions/View+Localized.swift
+++ Sources/AKLanguageManager/Extensions/View+Localized.swift
@@ -11,6 +11,6 @@ public extension View {
     func localized(in language: Language = AKLanguageManager.shared.selectedLanguage) -> some View {
         environment(\.locale, language.locale)
             .environment(\.layoutDirection, language.layoutDirection)
-            .id(AKLanguageManager.shared.observedLocalizer?.uuid ?? "")
+            .id(UUID().uuidString)
     }
 }
