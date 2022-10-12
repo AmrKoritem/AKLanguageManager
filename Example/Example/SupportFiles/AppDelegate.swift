e1fec4b3956ddbd9656cba5c4d3b15f8501e4c42
Amr Koritem <amr.koritem92@gmail.com>
2022-10-12 15:37:01 +0200

    example issue fixed

--- Example/Example/SupportFiles/AppDelegate.swift
+++ Example/Example/SupportFiles/AppDelegate.swift
@@ -12,7 +12,7 @@ import AKLanguageManager
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        AKLanguageManager.shared.defaultLanguage = .en
+        AKLanguageManager.shared.configureWith(defaultLanguage: .en)
         return true
     }
 }
